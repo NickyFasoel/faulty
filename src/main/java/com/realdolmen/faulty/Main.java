a49c97084e261fa9cba9c5834a75589b306f73af
Nicky Fasoel <Nicky.Fasoel@realdolmen.com>
2016-10-07 15:45:35 +0200

    fixed bug

--- src/main/java/com/realdolmen/faulty/Main.java
+++ src/main/java/com/realdolmen/faulty/Main.java
@@ -2,6 +2,6 @@ package com.realdolmen.faulty;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helo Wolrd!");
+        System.out.println("Hello World!");
     }
 }
